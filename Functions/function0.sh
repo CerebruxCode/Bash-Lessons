314e97c92041f7364da5fd93ec7b90d5159182e1
billniakas <billniakas@gmail.com>
2019-05-03 09:58:49 +0300

    Μέρος 6ο

---
+++ Functions/function0.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Βασική μορφή συνάρτησης
+
+print_something () {
+    echo "Είμαι ένα echo μεσα σε συνάρτηση"
+    }
+
+print_something
+
+
---
+++ Functions/function2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Χρήση ορίσματος σε συνάρτηση
+
+print_something () {
+	echo "Καλησπέρα $1"
+	    }
+
+print_something Μάκη
+print_something Βυργουνδία
+
